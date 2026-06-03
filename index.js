6e7cd6abdb507edca28ab305a043fe233fda2f4a
Casper21304 <valibulai1122@gmail.com>
2026-06-03 21:13:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req,res)=>{
+res.send("Bot online 🚀")
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=>console.log("WEB ON PORT", PORT))
 const {
 default: makeWASocket,
 useMultiFileAuthState,
